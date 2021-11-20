f390da94271e4d0d43d35e1ab5c3da36e9a62f93
miritzelichonok1 <miritz@startapp.com>
2021-11-20 16:26:28 +0200

    ml_course_files

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import sklearn as skl
+import matplotlib as mtp
+import statistics as st
+
+train = pd.read_excel('XY_train.xlsx')
+train.head()
+
+
+
+
